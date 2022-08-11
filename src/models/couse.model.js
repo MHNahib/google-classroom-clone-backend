842b4249d7b4cec85c19d657ba590581c429e60f
M. H. Nahib <mhnahib@gmail.com>
2022-08-11 11:54:11 +0600

    added course.model

---
+++ src/models/couse.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const couseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255,
+  },
+  teachersId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  students: {
+    type: Array,
+    required: true,
+  },
+});
+
+const Course = new mongoose.model("Course", couseSchema);
+
+module.exports.couseSchema = couseSchema;
+module.exports.Course = Course;
