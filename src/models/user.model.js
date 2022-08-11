77a925523a45a6b58d7fee7190c6211f7f1fc6e8
M. H. Nahib <mhnahib@gmail.com>
2022-08-11 11:42:36 +0600

    added user.model and joi for validation

---
+++ src/models/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255,
+  },
+  userType: {
+    type: String,
+    requried: true,
+    maxlength: 7,
+  },
+  email: {
+    type: String,
+    unique: true,
+    requried: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  uniqueId: {
+    type: String,
+    unique: true,
+  },
+});
+
+const User = new mongoose.model("User", userSchema);
+
+// joi validation
+const validation = (body) => {
+  const Schema = Joi.object({
+    name: Joi.string().min(3).max(255).required(),
+    userType: Joi.string().max(7).required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+  return schema.validate(body);
+};
+
+module.exports.userSchema = userSchema;
+module.exports.User = User;
+module.exports.validation = validation;
