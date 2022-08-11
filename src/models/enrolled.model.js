eb6ab05dfb037f7d676c0fac2877bd2acd82c2e9
M. H. Nahib <mhnahib@gmail.com>
2022-08-11 11:57:20 +0600

    added enrolled.model

---
+++ src/models/enrolled.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const enrolledSchema = new mongoose.Schema({
+  studentsId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Courses",
+    required: true,
+  },
+});
+
+const Enrolled = new mongoose.model("Enrolled", enrolledSchema);
+
+module.exports.couseSchema = enrolledSchema;
+module.exports.Enrolled = Enrolled;
